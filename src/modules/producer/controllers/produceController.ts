8e4958101dac0fc86c73862decbc9bc8d20a8613
Filizzola <juliofilizzola@hotmail.com>
2022-04-18 13:04:43 -0300

    🔇 : Removendo console.log

--- src/modules/producer/controllers/produceController.ts
+++ src/modules/producer/controllers/produceController.ts
@@ -5,8 +5,6 @@ import CreateProduce from "../services/CreateProduceService";
 class ProduceController {
   async execute(req: Request, res: Response) {
     const produce = container.resolve(CreateProduce);
-    console.log(req.body);
-    
     const { topic, message } = req.body;
     produce.execute({ topic, message });
     return res.json({ value: true })
