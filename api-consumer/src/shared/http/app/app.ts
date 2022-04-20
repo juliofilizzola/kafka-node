c045044f5ffd5f1c1fd658f412e505c259b738ef
Filizzola <juliofilizzola@hotmail.com>
2022-04-20 14:57:20 -0300

    ✨ : configurando banco de dados

--- api-consumer/src/shared/http/app/app.ts
+++ api-consumer/src/shared/http/app/app.ts
@@ -4,10 +4,11 @@ import express from 'express';
 import cors from 'cors';
 import routesAPP from '../routes/index.routes';
 import consumerService from '@kafka/consumer/consumer';
+import '@shared/typeorm';
 
 const app = express();
 
-consumerService();
+// consumerService();
 
 app.use(cors());
 app.use(routesAPP);
