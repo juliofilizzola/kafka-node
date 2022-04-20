3157592824dde77e6cff205216fb9a67d5430d15
Filizzola <juliofilizzola@hotmail.com>
2022-04-20 10:39:07 -0300

    🔥 : deletando condigos em conflito

--- src/kafka/consumer/loadConsumer.ts
+++
@@ -1,7 +0,0 @@
-const loadConsumer = ({ log, message }: any): void => {
-  console.log(log);
-  const result = Buffer.from(message.key, 'base64').toString('ascii');
-  console.log(result);
-};
-
-export default loadConsumer;
--- src/shared/http/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app/app';
-
-const port = 3000;
-const message = `init port: ${port}`;
-
-app.listen(port, () => console.log(message));
