9ae69cfeb8c8f5b0a7d1fd374bb356d0e1045ac1
Filizzola <juliofilizzola@hotmail.com>
2022-04-20 10:37:22 -0300

    🚚 : criando nova API: producer

---
+++ api-producer/src/modules/producer/services/CreateProduceService.ts
@@ -0,0 +1,11 @@
+import { IProducer } from '../../../kafka/dtos';
+import producerService from '../../../kafka/producer/producer';
+
+class CreateProduce {
+  async execute({ topic, message }: IProducer) {
+    const Produce = producerService({ topic, message });
+    return Produce;
+  }
+}
+
+export default CreateProduce;
---
+++ api-producer/src/shared/http/app/app.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import routesAPP from '../routes/index.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(routesAPP);
+
+export default app;
