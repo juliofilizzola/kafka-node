e84429e38bafe6353f6c137799a9f8a724f57375
Filizzola <juliofilizzola@hotmail.com>
2022-04-22 14:14:54 -0300

    🧐: modificando o arquivo de banco de dados.

--- api-consumer/src/database/data-base.ts
+++ api-consumer/src/database/data-base.ts
@@ -1,23 +1,23 @@
-import { DataSource } from 'typeorm';
-import Message from './entity/Message';
+// import { DataSource } from 'typeorm';
+// import Message from './entity/Message';
 
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'Kafka',
-  password: 'admin',
-  database: 'Kafka',
-  entities: [Message],
-  synchronize: true,
-  logging: false,
-});
+// const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'Kafka',
+//   password: 'admin',
+//   database: 'Kafka',
+//   entities: [Message],
+//   synchronize: true,
+//   logging: false,
+// });
 
-// to initialize initial connection with the database, register all entities
-// and "synchronize" database schema, call "initialize()" method of a newly created database
-// once in your application bootstrap
-AppDataSource.initialize()
-  .then(() => {
-    // here you can start to work with your database
-  })
-  .catch((error) => console.log(error));
+// // to initialize initial connection with the database, register all entities
+// // and "synchronize" database schema, call "initialize()" method of a newly created database
+// // once in your application bootstrap
+// AppDataSource.initialize()
+//   .then(() => {
+//     // here you can start to work with your database
+//   })
+//   .catch((error) => console.log(error));
